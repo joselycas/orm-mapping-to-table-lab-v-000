ed161a438b890c9e930438670587797d630da04d
Jos Castro <joselycas@gmail.com>
2018-08-13 17:29:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,8 +35,8 @@ class Student
   end
 
   def self.create(name:, grade:)
-   song = Song.new(name, grade)
-   song.save
-   song
+   student = Student.new(name, grade)
+   student.save
+   student
   end
 end
